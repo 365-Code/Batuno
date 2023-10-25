98073c3909b7ceb9ee5c3f7d6126ae1e79f189d2
365-Code <sohailahmedbhati786@gmail.com>
2023-10-25 13:42:53 +0530

    Integration

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const server = app.listen(process.env.PORT, ()=>{
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://batuno.vercel.app",
         credentials: true
     }
 })
